ca1c441ff5876ceaccf4eccd551103b9a133b03e
khris-xp <khrisbharmmano@gmail.com>
2023-06-22 22:57:48 +0700

    refactor : countdown hooks

---
+++ src/hooks/useCountdown.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useCountdown = (targetDate: string): number[] => {
+    const countDownDate = new Date(targetDate).getTime();
+    const [countDown, setCountDown] = useState<number>(
+        countDownDate - new Date().getTime()
+    );
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCountDown(countDownDate - new Date().getTime());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [countDownDate]);
+
+    const getReturnValues = (countDown: number): number[] => {
+        const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+            (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+        return [days, hours, minutes, seconds];
+    };
+
+    return getReturnValues(countDown);
+};
+
+export default useCountdown;
