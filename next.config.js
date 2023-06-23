f83b34960e1219fb694c244be1f2b7f7a4ab0cfa
BarwSirati <sirati10546@gmail.com>
2023-06-23 17:12:03 +0700

    chore: nextConfig

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
-        domains: ["localhost"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
       },
-}
+    ],
+    domains: ["localhost"],
+  },
+  reactStrictMode: true,
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
