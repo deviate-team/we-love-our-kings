41a641028156eb3a050c2645c78e77f1e09736cb
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-06-25 01:10:44 +0700

    fix: fix bug in api

--- src/app/api/kings/[id]/route.ts
+++ src/app/api/kings/[id]/route.ts
@@ -10,7 +10,6 @@ export async function GET(
     "https://raw.githubusercontent.com/deviate-team/we-love-our-kings/dev/src/data/kings.json"
   );
 
-
   const kings = await res.json();
 
   const king = kings.find((king: { id: number }) => king.id === +id);
--- src/app/api/kings/route.ts
+++ src/app/api/kings/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const res = await fetch("https://raw.githubusercontent.com/deviate-team/we-love-our-kings/dev/src/data/kings.json", {
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  })
+  const res = await fetch(
+    "https://raw.githubusercontent.com/deviate-team/we-love-our-kings/dev/src/data/kings.json"
+  );
 
   const kings = await res.json();
 
