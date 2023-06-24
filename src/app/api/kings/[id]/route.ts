61d9497f6cab55a985310bcaa55f810dc66be3c6
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-06-24 17:33:14 +0700

    chore: fetch king with id

--- src/app/api/kings/[id]/route.ts
+++ src/app/api/kings/[id]/route.ts
@@ -2,17 +2,18 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { name: string } }
+  { params }: { params: { id: string } }
 ) {
-  const { name } = params;
+  const { id } = params;
 
   const res = await fetch(
     "https://raw.githubusercontent.com/deviate-team/we-love-our-kings/dev/src/data/kings.json"
   );
 
+
   const kings = await res.json();
 
-  const king = kings.find((king: { name: string }) => king.name === name);
+  const king = kings.find((king: { id: number }) => king.id === +id);
 
   return NextResponse.json(king);
 }
