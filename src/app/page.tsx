b0b481799bf1735d5af37e384de208252669350f
khris-xp <khrisbharmmano@gmail.com>
2023-06-24 11:33:32 +0700

    feat: sidebar and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,170 @@
 "use client"
-import useCountdown from "@/hooks/useCountdown";
+import Banner from "@/components/Banner";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import { Fragment } from "react";
 
 export default function Home() {
-  let [days, hours, minutes, seconds]: number[] = useCountdown("June 25, 2023 9:30:00");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-[url('/background.jpeg')]">
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <div className="w-64 h-64 relative">
-          <Image src='https://king.kapook.com/kingrama10/images/king.png' alt="banner" fill />
+    <Fragment>
+      <Banner />
+      <Navbar>
+        <div className="container my-12 mx-auto px-4 md:px-12 mt-20">
+          <div className="flex flex-wrap -mx-1 lg:-mx-4">
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <article className="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <Image alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
+                </a>
+                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                  <h1 className="text-lg">
+                    <a className="no-underline hover:underline text-black" href="#">
+                      Article Title
+                    </a>
+                  </h1>
+                  <p className="text-grey-darker text-sm">
+                    11/1/19
+                  </p>
+                </header>
+
+                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                  <a className="flex items-center no-underline hover:underline text-black" href="#">
+                    <Image alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                    <p className="ml-2 text-sm">
+                      Author Name
+                    </p>
+                  </a>
+                  <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                    <span className="hidden">Like</span>
+                    <i className="fa fa-heart"></i>
+                  </a>
+                </footer>
+
+              </article>
+            </div>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <article className="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <Image alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
+                </a>
+                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                  <h1 className="text-lg">
+                    <a className="no-underline hover:underline text-black" href="#">
+                      Article Title
+                    </a>
+                  </h1>
+                  <p className="text-grey-darker text-sm">
+                    11/1/19
+                  </p>
+                </header>
+                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                  <a className="flex items-center no-underline hover:underline text-black" href="#">
+                    <Image alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                    <p className="ml-2 text-sm">
+                      Author Name
+                    </p>
+                  </a>
+                  <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                    <span className="hidden">Like</span>
+                    <i className="fa fa-heart"></i>
+                  </a>
+                </footer>
+              </article>
+            </div>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <article className="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <Image alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
+                </a>
+                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                  <h1 className="text-lg">
+                    <a className="no-underline hover:underline text-black" href="#">
+                      Article Title
+                    </a>
+                  </h1>
+                  <p className="text-grey-darker text-sm">
+                    11/1/19
+                  </p>
+                </header>
+
+                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                  <a className="flex items-center no-underline hover:underline text-black" href="#">
+                    <Image alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                    <p className="ml-2 text-sm">
+                      Author Name
+                    </p>
+                  </a>
+                  <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                    <span className="hidden">Like</span>
+                    <i className="fa fa-heart"></i>
+                  </a>
+                </footer>
+              </article>
+            </div>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <article className="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <Image alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
+                </a>
+                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                  <h1 className="text-lg">
+                    <a className="no-underline hover:underline text-black" href="#">
+                      Article Title
+                    </a>
+                  </h1>
+                  <p className="text-grey-darker text-sm">
+                    11/1/19
+                  </p>
+                </header>
+
+                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                  <a className="flex items-center no-underline hover:underline text-black" href="#">
+                    <Image alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                    <p className="ml-2 text-sm">
+                      Author Name
+                    </p>
+                  </a>
+                  <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                    <span className="hidden">Like</span>
+                    <i className="fa fa-heart"></i>
+                  </a>
+                </footer>
+              </article>
+            </div>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <article className="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
+                  <Image alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
+                </a>
+                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                  <h1 className="text-lg">
+                    <a className="no-underline hover:underline text-black" href="#">
+                      Article Title
+                    </a>
+                  </h1>
+                  <p className="text-grey-darker text-sm">
+                    11/1/19
+                  </p>
+                </header>
+
+                <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                  <a className="flex items-center no-underline hover:underline text-black" href="#">
+                    <Image alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                    <p className="ml-2 text-sm">
+                      Author Name
+                    </p>
+                  </a>
+                  <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                    <span className="hidden">Like</span>
+                    <i className="fa fa-heart"></i>
+                  </a>
+                </footer>
+              </article>
+            </div>
+          </div>
         </div>
-        <h1 className="text-5xl text-white font-bold mb-8">
-          รอทอดพระเนตรเร็วๆนี้
-        </h1>
-        <p>เหลืออีก 0{days} วัน {hours} ชั่วโมง {minutes} นาที {seconds} วินาที</p>
-      </div>
-    </main>
+      </Navbar>
+    </Fragment >
   )
 }
