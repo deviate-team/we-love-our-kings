760484e165bbd9a3b68a7fa7de35328f7cae8403
Fakepng <krit0kasemtewin@gmail.com>
2023-06-25 09:58:24 +0700

    feat: change away from arrow function

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,9 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Banner = () => {
-    return (
-        <Fragment>
-            <div className="bg-cover bg-center h-auto text-white py-40 px-10 object-fill bg-[url('/banner-main.jpeg')]" />
-        </Fragment>
-    )
+export default function Banner() {
+  return (
+    <Fragment>
+      <div className="bg-cover bg-center h-auto text-white py-40 px-10 object-fill bg-[url('/banner-main.jpeg')]" />
+    </Fragment>
+  );
 }
-
-export default Banner
